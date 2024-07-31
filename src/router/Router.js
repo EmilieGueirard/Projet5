d4d471a087f57a0538ff04470a5ca0c3fd1ccda6
Emilie Gueirard <emilie.gueirard@live.fr>
2024-07-31 12:41:13 +0200

    installation + initialisation React Router + création répertoire : /assets, /pages, /styles, /components, /router + installation Sass + Layout pour Header-Footer + structure minimale des pages fonctionnelles

---
+++ src/router/Router.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from '../pages/Home';
+import About from '../pages/About';
+import RealEstate from '../pages/RealEstate';
+import NotFound from '../pages/NotFound';
+import Layout from '../components/Layout'
+
+const Router = () => {
+    return (
+        <BrowserRouter>
+            <Layout>
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/apropos" element={<About />} />
+                    <Route path="/logement" element={<RealEstate />} />
+                    <Route path="*" element={<NotFound />} />
+                </Routes>
+            </Layout>
+        </BrowserRouter>
+    );
+};
+
+export default Router;
